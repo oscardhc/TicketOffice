dcf4515c2e42af26a64ac77475ec852a4cac4138
oscardhc <oscardhc@sjtu.edu.cn>
2019-05-11 23:27:00 +0800

    add basic buffer pool

--- backend/dhc/DataBase.hpp
+++ backend/dhc/DataBase.hpp
@@ -12,9 +12,6 @@
 #include <stdio.h>
 #include <fstream>
 
-#define BitSize (MAX_SIZE/32+1)
-
-template <class T, size_t MAX_SIZE>
 class DataBase {
     size_t BaseSize;
 
@@ -27,63 +24,20 @@ private:
 
 public:
     DataBase() {
-
     }
     void init(std::string filename) {
-        tot = 0;
-        BaseSize = (1 + BitSize) * sizeof(int);
-        memset(del, 0, sizeof(del));
-        file = std::fstream(filename, std::ios::app);
-        file.close();
-        file.clear();
-        file = std::fstream(filename, std::ios::in|std::ios::out|std::ios::binary);
-        file.seekp(0, std::ios::end);
-        if (file.tellg() == 0) {
-            file.seekp(0, std::ios::beg);
-            file.write((char*)&tot, sizeof(int));
-            file.write((char*)del, BitSize * sizeof(int));
-        } else {
-            file.seekp(0, std::ios::beg);
-            file.read((char*)&tot, sizeof(int));
-            file.read((char*)del, BitSize * sizeof(int));
-        }
     }
 
     ~DataBase() {
-        file.seekp(0, std::ios::beg);
-        file.write((char*)&tot, sizeof(int));
-        file.write((char*)del, BitSize * sizeof(int));
-        file.close();
     }
 
-    void getElement(T *t, size_t offset, size_t elementSize) {
-        file.seekp(BaseSize + offset, std::ios::beg);
-        file.read((char*)t, elementSize);
+    void getElement(char *t, size_t offset, size_t elementSize) {
     }
 
-    // return 0: ok, -1: error
-    int delElement(int index) {
-        int i = index / 32;
-        int j = index % 32;
-        if (del[i] & (1 << j)) {
-            del[i] ^= (1 << j);
-            return 0;
-        } else {
-            return -1;
-        }
+    size_t createElement() {
     }
 
-    // return the offset of the appended element
-    size_t appElement(T *t, size_t elementSize) {
-        file.seekp(0, std::ios::beg);
-        size_t pos = file.tellp();
-        file.write((char*)t, elementSize);
-        return pos;
-    }
-
-    void setElement(T *t, size_t offset, size_t elementSize) {
-        file.seekp(BaseSize + offset, std::ios::beg);
-        file.write((char*)t, elementSize);
+    void setElement(char *t, size_t offset, size_t elementSize) {
     }
 
 };
